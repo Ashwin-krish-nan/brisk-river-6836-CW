89ccd5bb7c0cea429edf60fb72288a07d9c29f35
Ashwin <107248597+Ashwin-krish-nan@users.noreply.github.com>
2023-01-22 18:52:57 +0530

    Update db.js
    added url straight

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose=require("mongoose")
 
 require('dotenv').config()
 
-const connection=mongoose.connect(process.env.mongoURL)
+const connection=mongoose.connect("mongodb+srv://ashwin:kavitha@cluster0.ivcxf6h.mongodb.net/limeroad?retryWrites=true&w=majority")
 
 
 
